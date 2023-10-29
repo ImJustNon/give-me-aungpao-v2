7e803364679568f4a6fca8d07e46eaccbb056136
ImJustNon <Im_Just_Non@hotmail.com>
2023-10-29 22:01:11 +0700

    Feat : Add API system

--- src/pages/api/truewallet/redeem.js
+++ src/pages/api/truewallet/redeem.js
@@ -8,58 +8,17 @@ async function handler(req, res) {
 
 	const { url } = req.body ?? {};
 
-	if(!url){
+	axios.post("https://giveme.nonlnwza.xyz/api/redeem", {
+		url: url,
+		number: config.api.truewallet.phoneNumber,
+	}).then(response =>{
+		return res.json(response);
+	}).catch(e => {
 		return res.json({
 			status: "FAIL",
-			error: "You have to provided voucher url",
+			error: e,
 		});
-	}
-
-	await redeemVoucher(config.api.truewallet.phoneNumber, url);
-
-	async function redeemVoucher(phoneNumber = "", voucherUrl = ""){
-		if(!(phoneNumber = (phoneNumber + "").trim()).length || phoneNumber.match(/\D/)){ 
-			return res.json({
-				status: "FAIL",
-				error: "INVAILD_PHONENUMBER"	
-			});
-		}
-		let r = (voucherUrl += "").split("v=");
-		if(18 != (voucherUrl = (r[1] || r[0]).match(/[0-9A-Za-z]+/)[0]).length){ 
-			return res.json({
-				status: "FAIL",
-				error: "INVAILD_VOUCHER"	
-			});
-		}
-
-		axios.post(`https://gift.truemoney.com/campaign/vouchers/${voucherUrl}/redeem`, {
-			mobile: phoneNumber,
-			voucher_hash: voucherUrl,
-		}, {
-			headers: {
-			 	'Content-Type': 'application/json'
-			}
-		}).then(response => {
-			console.log(response.status);
-			if(response.status.code == "SUCCESS"){
-				return res.json({
-					status: "SUCCESS",
-					error: null,
-					data: {
-						amount: Number(response.data.my_ticket.amount_baht.replace(/,/g, '')),
-						voucherOwner: response.data.owner_profile.full_name, 
-						voucherUrl: voucherUrl,
-					}	
-				});
-			}
-			else {
-				return res.json({
-					status: "FAIL",
-					error: response.status.code
-				});
-			}
-		}).catch(e => {});
-	}
+	});
 }
 
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,29 @@
 import Image from 'next/image'
 import React, { useEffect, useState } from "react";
 import Router from 'next/router'
-
+import axios from 'axios';
 
 function Home() {
+	const [voucherUrl, setVoucherUrl] = useState("");
+	const [errorMessage, setErrorMessage] = useState("");
 
 	function handleSubmit(){
-		Router.push('/thankyou?name=nonlnwza&amount=100');
+		axios.post("/api/truewallet/redeem", {
+			url: voucherUrl,
+		}).then(response =>{
+			if(response.data.error){
+				setErrorMessage("🤬 What did you send me? I only need money. 💵");
+			}
+			else {
+				Router.push(`/thankyou?name=${response.data.data.from}&amount=${response.data.data.amount}`);
+			}
+		}).catch(() => {
+			setErrorMessage("Request problem please try again later. na 😅");
+		});
+	}
+	function handleInput(event){
+		setVoucherUrl(event.target.value);
+		setErrorMessage("");
 	}
 
 	return (
@@ -17,8 +34,13 @@ function Home() {
 						<h1 className="text-3xl font-bold mb-2">🧧 Give Me Aungpao. NOW!!! 🧧</h1>
 						<p className='text-lg mb-1'>🤑 Have you ever felt like you didn{"'"}t know what to do with your money?  🤑</p>
 						<p className='text-sm mb-5'>Just bring it to me. LOL</p>
-						<input type="text" placeholder="Paste Your Money Here" className="input input-bordered input-error text-center w-full max-w-lg mr-2" />
+						<input type="text" placeholder="Paste Your Money Here" className="input input-bordered input-error text-center w-full max-w-lg mr-2" onChange={(e) => handleInput(e)} />
 						<button className="btn bg-orange-600 btn-error text-white font-normal normal-case mt-3 w-96 md:w-auto" onClick={(event) => handleSubmit(event)} >ByeBye <i className="fa-solid fa-chevron-right"></i></button>
+						{errorMessage ? 
+							<p className='text-error text-sm mt-3'>{errorMessage}</p> 
+							: 
+							<></>
+						}
 					</div>
 				</div>
 			</div>
