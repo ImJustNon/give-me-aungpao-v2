38bfacc9cca0072b8ee6c2b066746e3e03160e6d
ImJustNon <Im_Just_Non@hotmail.com>
2023-11-05 20:41:56 +0700

    Feat : Change to Link tag

--- src/pages/thankyou/index.js
+++ src/pages/thankyou/index.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import Router from "next/router";
 import { useEffect, useState } from "react";
 import { useSearchParams } from 'next/navigation';
+import Link from 'next/link';
 
 function Thankyou(){
 	const [name, setName] = useState("");
@@ -15,10 +16,6 @@ function Thankyou(){
 		setAmount(getAmount);
 	}, [searchParams]);
 
-	function handleGoBack(){
-		Router.push("/");
-	}
-
     return (
         <>
             <div className="hero min-h-screen">
@@ -27,7 +24,7 @@ function Thankyou(){
 						<h1 className="text-2xl font-bold mb-2">😍 Thank you <span className="font-bold text-4xl text-error">{name}</span> for being scammed <span className="font-bold text-4xl text-error">{amount}</span> Baht. 💸</h1>
 						<p className="text-lg mb-1">🤔 Still can{"'"}t figure out what to do with the money? 🤔</p>
 						<p className="text-sm mb-5">Just click the button below. LOL</p>
-						<button className="btn bg-orange-600 btn-error text-white font-normal normal-case mt-3 w-96 md:w-auto" onClick={(event) => handleGoBack(event)} ><i className="fa-solid fa-chevron-left"></i> Go ByeBye Again</button>
+						<Link className="btn bg-orange-600 btn-error text-white font-normal normal-case mt-3 w-96 md:w-auto" href="/" ><i className="fa-solid fa-chevron-left"></i> Go ByeBye Again</Link>
 					</div>
 				</div>
 			</div>
